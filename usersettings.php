8c3af36381a8f4beef855d696f623f1f5999aa57
whyboris <yboris@yahoo.com>
2017-01-29 22:01:21 -0500

    moved code from one file into another - into appropriate files

--- usersettings.php
+++ usersettings.php
@@ -6,13 +6,13 @@ include('metaboxfactory.php');
 
 require('connection.php');
 
+include('usersettingsfactory.php');
+
 // grab user's settings from database
-
-    // later will be an ID -- at the moment just use the NAME
-    $id = $_SESSION['name'];
-
-    $query = array('name'=> $id);
-    $userSettings = $userCollection->findOne($query);
+// later will be an ID -- at the moment just use the NAME
+$id = $_SESSION['name'];
+$query = array('name'=> $id);
+$userSettings = $userCollection->findOne($query);
 
 ?>
 
@@ -28,103 +28,41 @@ require('connection.php');
 
 <?php
 
-function renderUserSetting($displayName, $fieldName, $value, $optionsArray, $selectionType){
-    echo '<div class="form-group">';
+$contribTypeArray = array(
+    'contributor' => 'Contributor',
+    'editor' => 'Editor',
+    'staff' => 'Staff',
+);
 
-        echo '<label class="control-label col-sm-3" for="name">'.$displayName.'</label>';
+$deptArray = array(
+    'dev' => 'Developers',
+    'art' => 'Art',
+    'editorial' => 'Editorial',
+);
 
-        echo '<div class="col-sm-9">';
+$privilegeArray = array(
+    'coffee' => 'Free coffee',
+    'icecream' => 'Free ice cream',
+    'cookies' => 'Free cookies',
+);
 
-        // convert to SWITCH / CASE
-        if ($selectionType === null) {
-            echo '<input type="text"  class="form-control" name="'.$fieldName.'" value="'.$value.'">';
-        } elseif ($selectionType == 'dropdown') {
-            echo '<div class="selectpicker">';
-            echo '<select class="form-control" name="'. $fieldName . '">';
-            foreach ($optionsArray as $key => $value2) {
-                $selected = '';
-                if ($value == $key) {
-                    $selected = 'selected';
-                }
-                echo '<option value="'.$key.'" '.$selected.'>'.$value2.'</option>';
-            }
-            echo '</select>';
-            echo '</div>';
-        } elseif ($selectionType == 'radio') {
-            foreach ($optionsArray as $key => $value2) {
+// Register Fields for Group 1
+registerUserMeta('First Name', 'name', 1);
+registerUserMeta('Last Name', 'lastName', 1);
+registerUserMeta('Twitter', 'twitter', 1);
+registerUserMeta('Facebook', 'facebook', 1);
+registerUserMeta('Instagram', 'instagram', 1);
 
-                $selected = '';
-                if ($value == $key) {
-                    $selected = 'checked';
-                }
+// Register Fields for Group 2
+registerUserMeta('Contributor Type', 'type', 2, $contribTypeArray, 'dropdown');
+registerUserMeta('Salary', 'salary', 2);
+registerUserMeta('Department', 'department', 2, $deptArray, 'radio');
+registerUserMeta('Privileges', 'privilege', 2, $privilegeArray, 'checkboxes');
 
-                echo '<div class="radio">';
-                echo '<label><input type="radio" name="' . $fieldName . '" value="' . $key . '" '.$selected.'>' . $value2 . '</label>';
-                echo '</div>';
-            }
-        } elseif ($selectionType == 'checkboxes') {
-
-            foreach ($optionsArray as $key => $value3) {
-
-                $selected = '';
-
-                $chosenValue = $value;
-                // chosen value can be an array!
-                if (isset($chosenValue) && $chosenValue != '') {
-                    foreach ($chosenValue as $key2 => $value2) {
-                        if ($key == $value2) {
-                            $selected = 'checked';
-                        }
-                    }
-                }
-
-                echo '<div class="checkbox">';
-                echo '<label><input type="checkbox" name="' . $fieldName . '[]" value="'. $key . '" '.$selected.'>' . $value3 . '</label>';
-                echo '</div>';
-            }
-        }
-
-
-        echo '</div>';
-
-    echo '</div>';
-}
-
-include('usersettingsfactory.php');
-
-/**
- * Render all the user settings
- *
- * Separates into groups
- * @param  string   $groupDisplayName   Group name to display
- * @param  string   $groupDivId         Div id for jQuery to grab (not needed yet)
- * @param  string   $groupNumber        Render out only user meta for this group number
- * @return void
- */
-function displayUserSettingsGroup($groupDisplayName, $groupDivId, $groupNumber) {
-
-    metaboxPrefix($groupDisplayName, $groupDivId);
-
-    global $allUserMeta;
-    global $userSettings;
-
-    // cycle through all the registered user meta
-    foreach ($allUserMeta as $key => $value) {
-        if ($value[2] == $groupNumber) {
-            if (isset($userSettings[$value[1]])){
-                renderUserSetting($value[0], $value[1], $userSettings[$value[1]], $value[3], $value[4]);
-            } else {
-                renderUserSetting($value[0], $value[1], '', $value[3], $value[4]);
-            }
-        }
-    }
-
-    metaboxSuffix();
-
-}
-
+// Display Group 1
 displayUserSettingsGroup('Personal', 'personalMetabox', 1);
 
+// Display Group 2
 displayUserSettingsGroup('Professional', 'professionalMetabox', 2);
 
 
--- usersettingsfactory.php
+++ usersettingsfactory.php
@@ -16,36 +16,102 @@ function registerUserMeta($displayName, $fieldName, $groupId = 1, $optionsArray 
     array_push($allUserMeta, array($displayName, $fieldName, $groupId, $optionsArray, $selectionType));
 }
 
-$contribTypeArray = array(
-    'contributor' => 'Contributor',
-    'editor' => 'Editor',
-    'staff' => 'Staff',
-);
+function renderUserSetting($displayName, $fieldName, $value, $optionsArray, $selectionType){
+    echo '<div class="form-group">';
 
-$deptArray = array(
-    'dev' => 'Developers',
-    'art' => 'Art',
-    'editorial' => 'Editorial',
-);
+        echo '<label class="control-label col-sm-3" for="name">'.$displayName.'</label>';
 
-$privilegeArray = array(
-    'coffee' => 'Free coffee',
-    'icecream' => 'Free ice cream',
-    'cookies' => 'Free cookies',
-);
+        echo '<div class="col-sm-9">';
 
-// Group 1
-registerUserMeta('First Name', 'name', 1);
-registerUserMeta('Last Name', 'lastName', 1);
-registerUserMeta('Twitter', 'twitter', 1);
-registerUserMeta('Facebook', 'facebook', 1);
-registerUserMeta('Instagram', 'instagram', 1);
+        // convert to SWITCH / CASE
+        if ($selectionType === null) {
+            echo '<input type="text"  class="form-control" name="'.$fieldName.'" value="'.$value.'">';
+        } elseif ($selectionType == 'dropdown') {
+            echo '<div class="selectpicker">';
+            echo '<select class="form-control" name="'. $fieldName . '">';
+            foreach ($optionsArray as $key => $value2) {
+                $selected = '';
+                if ($value == $key) {
+                    $selected = 'selected';
+                }
+                echo '<option value="'.$key.'" '.$selected.'>'.$value2.'</option>';
+            }
+            echo '</select>';
+            echo '</div>';
+        } elseif ($selectionType == 'radio') {
+            foreach ($optionsArray as $key => $value2) {
 
-// Group 2
-registerUserMeta('Contributor Type', 'type', 2, $contribTypeArray, 'dropdown');
-registerUserMeta('Salary', 'salary', 2);
-registerUserMeta('Department', 'department', 2, $deptArray, 'radio');
-registerUserMeta('Privileges', 'privilege', 2, $privilegeArray, 'checkboxes')
+                $selected = '';
+                if ($value == $key) {
+                    $selected = 'checked';
+                }
+
+                echo '<div class="radio">';
+                echo '<label><input type="radio" name="' . $fieldName . '" value="' . $key . '" '.$selected.'>' . $value2 . '</label>';
+                echo '</div>';
+            }
+        } elseif ($selectionType == 'checkboxes') {
+
+            foreach ($optionsArray as $key => $value3) {
+
+                $selected = '';
+
+                $chosenValue = $value;
+                // chosen value can be an array!
+                if (isset($chosenValue) && $chosenValue != '') {
+                    foreach ($chosenValue as $key2 => $value2) {
+                        if ($key == $value2) {
+                            $selected = 'checked';
+                        }
+                    }
+                }
+
+                echo '<div class="checkbox">';
+                echo '<label><input type="checkbox" name="' . $fieldName . '[]" value="'. $key . '" '.$selected.'>' . $value3 . '</label>';
+                echo '</div>';
+            }
+        }
+
+
+        echo '</div>';
+
+    echo '</div>';
+}
+
+
+/**
+ * Render all the user settings
+ *
+ * Separates into groups
+ * @param  string   $groupDisplayName   Group name to display
+ * @param  string   $groupDivId         Div id for jQuery to grab (not needed yet)
+ * @param  string   $groupNumber        Render out only user meta for this group number
+ * @return void
+ */
+function displayUserSettingsGroup($groupDisplayName, $groupDivId, $groupNumber) {
+
+    metaboxPrefix($groupDisplayName, $groupDivId);
+
+    global $allUserMeta;
+    global $userSettings;
+
+    // cycle through all the registered user meta
+    foreach ($allUserMeta as $key => $value) {
+        if ($value[2] == $groupNumber) {
+            if (isset($userSettings[$value[1]])){
+                renderUserSetting($value[0], $value[1], $userSettings[$value[1]], $value[3], $value[4]);
+            } else {
+                renderUserSetting($value[0], $value[1], '', $value[3], $value[4]);
+            }
+        }
+    }
+
+    metaboxSuffix();
+
+}
+
+
+
 
 
  ?>
