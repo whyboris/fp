150dac152a3564ac1bff96810845cd12cd369af6
whyboris <yboris@yahoo.com>
2017-02-04 11:32:05 -0500

    working version of summernote -- saving and restoring saved content

--- assets/custom.js
+++ assets/custom.js
@@ -1,12 +1,27 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
 
-    //console.log( "ready!" );
+    console.log("custom.js running");
 
 
     $("#allOptions").find(':checkbox').click(function(){
         console.log($(this).val());
         $('#' + $(this).val()).toggle();
     })
+
+    $('#thePostForm').submit(function() {
+        validateForm();
+    });
+
+    function validateForm() {
+        var markupStr = $('#summernote').summernote('code');
+        $('#thePostContents').val(markupStr);
+        //console.log("Current contents of summernote:");
+        //console.log(markupStr);
+    }
+
+    // Put contents of past save into summernote post
+    previouslySavedText = $('#thePostContents').val();
+    $('#summernote').summernote('code', previouslySavedText);
 
 });
